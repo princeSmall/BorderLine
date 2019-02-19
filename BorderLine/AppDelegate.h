306dc7cb0a03efbf31ac775fce60a9cc99ff5929
le tong <1263512924@qq.com>
2019-02-19 17:17:50 +0800

    Initial Commit

---
+++ BorderLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BorderLine
+//
+//  Created by le tong on 2019/2/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BorderLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BorderLine
+//
+//  Created by le tong on 2019/2/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
